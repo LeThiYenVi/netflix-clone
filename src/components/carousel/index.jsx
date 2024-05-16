120db696a9ba0256882c20818ae7224eefa89739
Vi <viltyse182544@fpt.edu.vn>
2024-05-16 20:19:03 +0700

    updatee

--- src/components/carousel/index.jsx
+++ src/components/carousel/index.jsx
@@ -4,18 +4,25 @@ import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 import "./index.scss";
 
 // import required modules
-import { Pagination } from "swiper/modules";
+import { Pagination, Autoplay, Navigation } from "swiper/modules";
 import axios from "axios";
 import { useEffect, useState } from "react";
+
 //Props
 //numberOfSlide
 //
 
-export default function Carousel({ numberOfSlide, category }) {
+export default function Carousel({
+  numberOfSlide,
+  category,
+  isUseNavigation = false,
+  title,
+}) {
   const [movies, setMovies] = useState([]);
   const fetchMovies = async () => {
     const response = await axios.get(
@@ -28,30 +35,29 @@ export default function Carousel({ numberOfSlide, category }) {
     fetchMovies();
   }, []);
   return (
-    <>
+    <div className={`carousel ${numberOfSlide > 1 ? "multi-item" : ""}`}>
+      {/* Chi show tittle khi va chi khi co title => neu tittle === nill => kh show tittle */}
+      {title && <h1>{title}</h1>}
       <Swiper
+        navigation={isUseNavigation}
         slidesPerView={numberOfSlide}
         autoplay={{
-          
           delay: 2500,
           disableOnInteraction: false,
         }}
         pagination={true}
-        modules={[Pagination]}
-        className="carousel"
+        modules={[Pagination, Autoplay, Navigation]}
       >
         {/* movie => swiperslide */}
         {/* cu moi movie trong movies => swiperslide */}
-        {movies.filter(movie => movie.category === category).map(movie =><SwiperSlide key={movie.id}>
-          <img
-            src={movie.poster_path}
-            alt=""
-          />
-        </SwiperSlide>)}
-        
-
-        
+        {movies
+          .filter((movie) => movie.category === category)
+          .map((movie) => (
+            <SwiperSlide key={movie.id}>
+              <img src={movie.poster_path} alt="" />
+            </SwiperSlide>
+          ))}
       </Swiper>
-    </>
+    </div>
   );
 }
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,14 +2,19 @@ import Carousel from "../../components/carousel";
 import Header from "../../components/header";
 
 function HomePage() {
-  return <div>
-    <Header />
-    <Carousel numberOfSlide={1} category={"Trending"} />
-    <Carousel numberOfSlide={6} category={"Horror"} />
-    <Carousel numberOfSlide={6} category={"Comedy"} />
-
-
-  </div>
+  return (
+    <div>
+      <Header />
+      <Carousel numberOfSlide={1} category={"Trending"} />
+      <Carousel
+        numberOfSlide={6}
+        category={"Horror"}
+        isUseNavigation
+        title="Horror Movies"
+      />
+      <Carousel numberOfSlide={6} category={"Comedy"} isUseNavigation={true} />
+    </div>
+  );
 }
 
 export default HomePage;
